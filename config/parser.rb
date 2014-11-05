67a6ec8252f046057efb69508d97aea0e078a402
Matthew <matthewmalloch@yahoo.co.uk>
2014-11-05 12:00:27 +0000

    testing branch

--- config/parser.rb
+++ config/parser.rb
@@ -14,34 +14,23 @@ f.each do |line|
   end
   
   #gets control
-  firstcomma = line.index(',')
-  stringf = line[0,line.index(',')]
-  stringe = line[line.index(','),line.length-line.index(',')]
-  control = stringf
-  line = stringe
+  control = line[0,line.index(',')]
+  line = line[line.index(','),line.length-line.index(',')]
   
   #gets loans
-  lastcomma = line.rindex(',')
-  stringf = line[1,line.rindex(',')-1]
-  stringe = line[line.rindex(',')+1,line.length-line.rindex(',')-1]
-  loans=stringe.to_i()
-  line = stringf
+  loans=line[line.rindex(',')+1,line.length-line.rindex(',')-1].to_i()
+  line = line[1,line.rindex(',')-1]
   
 #gets writer
-  slash = line.index('/')
-  author = line[slash+2, line.length-line.index('/')]
+  author = line[line.index('/')+2, line.length-line.index('/')]
 
 #removes unused data
   line = line[line.rindex(',')+1,line.length-line.rindex(',')-1]
   
  #gets title
   title = line.split('/')[0]
-    
-  puts control
+  
   puts title 
-  puts author
-  puts loans
-  puts '/'
   
   @books = Book.new
   @books.control = control
